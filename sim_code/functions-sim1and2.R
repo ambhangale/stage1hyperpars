5509f4ca117188507262e15a9e074a8d9fb34937
ambhangale <148333008+ambhangale@users.noreply.github.com>
2024-03-25 12:38:01 +0100

    create simulation 1 & 2 functions file

---
+++ sim_code/functions-sim1and2.R
@@ -0,0 +1,12 @@
+## Aditi M. Bhangale
+## Last updated: 25 March 2024
+
+# Hyperparameters of empirical Bayes priors for MCMC estimation of the 
+# multivariate social relations model
+# Simulations 1 and 2
+
+# rm(list = ls())
+
+# setwd("/Users/Aditi_2/Desktop/UvA/SR-SEM_job/stage1hyperpars/")
+# getwd()
+
