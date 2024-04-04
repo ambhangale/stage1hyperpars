b7124bd1a8be8dadb77e23c63ecb9acd3943bef2
ambhangale <148333008+ambhangale@users.noreply.github.com>
2024-04-04 14:57:29 +0200

    adjust time; limit sim to one thread

--- sim_code/shell-sim1and2.sh
+++ sim_code/shell-sim1and2.sh
@@ -5,12 +5,13 @@
 #SBATCH -o sim1and2.SOUT
 #SBATCH -N 1
 #SBATCH --cpus-per-task 32
-#SBACTH -t 12-20:00:00
+#SBACTH -t 3-20:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=aditibhangale@gmail.com
 
 cd $SLURM_SUBMIT_DIR
 
 module load R/4.2.2
+export MKL_NUM_THREADS=1
 
 Rscript --vanilla runsim-sim1to3.R
