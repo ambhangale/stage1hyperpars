52ce20968923256a7bf7d771d7a596cb54f21253
ambhangale <148333008+ambhangale@users.noreply.github.com>
2024-04-04 11:58:04 +0200

    shell for simulations 1 and 2

---
+++ sim_code/shell-sim1and2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -J sim1and2
+#SBATCH -e sim1and2.SERR
+#SBATCH -o sim1and2.SOUT
+#SBATCH -N 1
+#SBATCH --cpus-per-task 32
+#SBACTH -t 12-20:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=aditibhangale@gmail.com
+
+cd $SLURM_SUBMIT_DIR
+
+module load R/4.2.2
+
+Rscript --vanilla runsim-sim1to3.R
