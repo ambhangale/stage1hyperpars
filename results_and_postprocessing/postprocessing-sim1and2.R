85f18dc50cc3b3bca055c951c5ec1d763d565013
ambhangale <148333008+ambhangale@users.noreply.github.com>
2024-04-08 11:40:48 +0200

    postprocessing files

---
+++ results_and_postprocessing/postprocessing-sim1and2.R
@@ -0,0 +1,11 @@
+## Aditi M. Bhangale
+## Last updated: 8 April 2024
+
+# Hyperparameters of empirical Bayes priors for MCMC estimation of the 
+# multivariate social relations model
+# Simulations 1 to 3
+
+# rm(list = ls())
+
+# setwd("/Users/Aditi_2/Desktop/UvA/SR-SEM_job/stage1hyperpars/sim_code")
+# getwd()
