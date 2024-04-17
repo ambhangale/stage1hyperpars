58c318e90cb7eb60710aed3cd5f41bc8f75b3768
ambhangale <148333008+ambhangale@users.noreply.github.com>
2024-04-17 16:00:40 +0200

    add file for postprocessing sim1

--- results_and_postprocessing/postprocessing-sim1.R
+++ results_and_postprocessing/postprocessing-sim1.R
@@ -3,11 +3,12 @@
 
 # Hyperparameters of empirical Bayes priors for MCMC estimation of the 
 # multivariate social relations model
-# Simulations 1 to 3
 
-# POSTPROCESSING
+# POSTPROCESSING for simulation 1
 
 # rm(list = ls())
 
 # setwd("/Users/Aditi_2/Desktop/UvA/SR-SEM_job/stage1hyperpars/results_and_postprocessing")
 # getwd()
+
+
